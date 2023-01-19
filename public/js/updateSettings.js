bd089b913a293c04b8d949f50ea8c51ea309c2fd
thien <nguyenthanhthien0507@gmail.com>
2023-01-19 21:38:45 +0700

    finish

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -5,8 +5,8 @@ export const updateSettings = async (data, type) => {
   try {
     const url =
       type === 'password'
-        ? '/api/v1/users/updateMyPassword'
-        : '/api/v1/users/updateMe';
+        ? 'http://localhost:8000/api/v1/users/updateMyPassword'
+        : 'http://localhost:8000/api/v1/users/updateMe';
     const res = await axios({
       method: 'PATCH',
       url,
